0d28aff6ecefa9998792ba4d0a17ff1ea88a6ec7
RazorzMcLazorz <nateyoungdev@gmail.com>
2022-04-24 15:39:43 -0600

    react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,15 @@
 import { isMobile } from 'react-device-detect'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import NoAccess from './components/noAccess/NoAccess'
-import logo from './logo.svg'
-import style from './App.module.scss'
+import HomePage from './components/home/HomePage'
 
 function App() {
   return isMobile ? (
-    <div className={style.App}>
-      <header className={style['App-header']}>
-        <img src={logo} className={style['App-logo']} alt='logo' />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className={style['App-link']}
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'>
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
   ) : (
     <NoAccess />
   )
